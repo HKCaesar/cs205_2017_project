b6d47c478797b633f08d8bc8d383f50c6c9d49d0
charlloyd <char.lloyd@gmail.com>
2017-03-30 19:55:12 -0400

    Update mpi4pycuda_hello.py

--- examples/ex-mpi4pycuda/mpi4pycuda_hello.py
+++ examples/ex-mpi4pycuda/mpi4pycuda_hello.py
@@ -1,16 +1,65 @@
 #!/usr/bin/env python
 
+"""
+Demo of how to pass GPU memory managed by pycuda to mpi4py.
+Notes
+-----
+This code can be used to perform peer-to-peer communication of data via
+NVIDIA's GPUDirect technology if mpi4py has been built against a
+CUDA-enabled MPI implementation.
+"""
+
+import atexit
+import sys
+
+# PyCUDA 2014.1 and later have built-in support for wrapping GPU memory with a
+# buffer interface:
+import pycuda
+if pycuda.VERSION >= (2014, 1):
+    bufint = lambda arr: arr.gpudata.as_buffer(arr.nbytes)
+else:
+    import cffi
+    ffi = cffi.FFI()
+    bufint = lambda arr: ffi.buffer(ffi.cast('void *', arr.ptr), arr.nbytes)
+
+import numpy as np
 from mpi4py import MPI
 
-nproc = MPI.COMM_WORLD.Get_size()   # Size of communicator 
-iproc = MPI.COMM_WORLD.Get_rank()   # Ranks in communicator
-inode = MPI.Get_processor_name()    # Node where this MPI process runs
+import pycuda.driver as drv
+import pycuda.gpuarray as gpuarray
+drv.init()
 
-if iproc == 0: print "This code is a test for mpi4py."
+def dtype_to_mpi(t):
+    if hasattr(MPI, '_typedict'):
+        mpi_type = MPI._typedict[np.dtype(t).char]
+    elif hasattr(MPI, '__TypeDict__'):
+        mpi_type = MPI.__TypeDict__[np.dtype(t).char]
+    else:
+        raise ValueError('cannot convert type')
+    return mpi_type
 
-for i in range(0,nproc):
-    MPI.COMM_WORLD.Barrier()
-    if iproc == i:
-        print 'Hello world from %d out of %d!' % (iproc,nproc)
-        
-MPI.Finalize()
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+N_gpu = drv.Device(0).count()
+if N_gpu < 2:
+    sys.stdout.write('at least 2 GPUs required')
+else:
+    dev = drv.Device(rank)
+    ctx = dev.make_context()
+    atexit.register(ctx.pop)
+    atexit.register(MPI.Finalize)
+
+    if rank == 0:
+        x_gpu = gpuarray.arange(100, 200, 10, dtype=np.double)
+        print ('before (%i): ' % rank)+str(x_gpu)
+        comm.Send([bufint(x_gpu), dtype_to_mpi(x_gpu.dtype)], dest=1)
+        print 'sent'
+        print ('after  (%i): ' % rank)+str(x_gpu)
+    elif rank == 1:
+        x_gpu = gpuarray.zeros(10, dtype=np.double)
+        print ('before (%i): ' % rank)+str(x_gpu)
+        comm.Recv([bufint(x_gpu), dtype_to_mpi(x_gpu.dtype)], source=0)
+        print 'received'
+        print ('after  (%i): ' % rank)+str(x_gpu)
