98d40ef024980273979b9f93135db5e8c2bf66b1
charlloyd <char.lloyd@gmail.com>
2017-03-30 19:02:23 -0400

    Update setup.sh

--- examples/ex-omp/setup.sh
+++ examples/ex-omp/setup.sh
@@ -1,3 +1,3 @@
 source new-modules.sh
 module load intel/15.0.0-fasrc01
-srun --mem-per-cpu=1000 -p gpu -n 1 --gres=gpu:1 --pty -t 0-01:00 /bin/bash
+srun --mem-per-cpu=1000 -p gpu -n 1 --gres=gpu:1 --constraint=cuda-7.5 --pty -t 0-01:00 /bin/bash
