0ac266f6ae28cb95513287c25a81661c150885d2
charlloyd <char.lloyd@gmail.com>
2017-03-30 20:14:19 -0400

    Update setup.sh

--- examples/ex-mpi4pycuda/setup.sh
+++ examples/ex-mpi4pycuda/setup.sh
@@ -1,5 +1,6 @@
 source new-modules.sh
 module load python/2.7.6-fasrc01
 module load pycuda/2015.1.3-fasrc01
+module load intel/15.0.0-fasrc01
 module load cuda/7.5-fasrc01
 srun --mem-per-cpu=1000 -p gpu -n 1 --gres=gpu:2 --constraint=cuda-7.5 --pty -t 0-01:00 /bin/bash
