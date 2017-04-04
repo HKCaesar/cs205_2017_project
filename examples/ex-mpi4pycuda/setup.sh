3ea2a1b40f816840852a71eb67cae4d2e3223c89
charlloyd <char.lloyd@gmail.com>
2017-04-04 17:16:56 -0400

    Update setup.sh

--- examples/ex-mpi4pycuda/setup.sh
+++ examples/ex-mpi4pycuda/setup.sh
@@ -2,5 +2,5 @@ module load python/2.7.11-fasrc01
 source activate pycuda
 module load cuda/7.5-fasrc02
 pip install pycuda
-module load gcc/4.9.0-fasrc01 openmpi/1.10.2-fasrc01
+module load gcc/4.8.2-fasrc01 openmpi/1.10.2-fasrc01
 pip install mpi4py
