48b9d49e96f9260f7a3182f2d9c097604ffbb2c0
Eric Dunipace <eifer@Cid.local>
2017-04-07 02:47:58 -0400

    added c wrapper via cython. confirmed gives same answer as karrems code for cluster assignemtn. Giving occasional segfaults and not producing cluster centers for some reason.

--- analysis/kM/src/main.c
+++ analysis/kM/src/main.c
@@ -11,33 +11,39 @@
 
 
 int main(){
-    float *x;
-    float *c;
+    double *x;
+    double *c;
     int *assign;
     int N = 100;
     int K = 5;
     int D = 5;
     
-    x = (float*) malloc(sizeof(float) * N * D);
-    c = (float*) malloc(sizeof(float) * K * D);
+    x = (double*) malloc(sizeof(double) * N * D);
+    c = (double*) malloc(sizeof(double) * K * D);
     assign = (int*) malloc(sizeof(int) * N);
     
     srand ( time(NULL) );
     
     for(int i = 0; i < (N*D); ++i) x[i] = rand();
-    for(int i = 0; i < (K*D); ++i) c[i] = rand();
+    for(int i = 0; i < (K*D); ++i) c[i] = 0;
     for(int i = 0; i < (N/K); ++i) for(int kk=0; kk < 5; ++kk) assign[kk + i * K] = kk;
     
-    for(int n =0; n < N; n++) printf("%d", assign[n]);
+//    for(int n =0; n < N; n++) printf("%d", assign[n]);
 //    for(int i = 0; i < (N*D); ++i){
 //        printf("%f", x[i]);
 //        printf(", ");
 //    }
-    printf("\n");
+//    printf("\n");
     
     kMeans (x, c, assign, N, K, D);
     
-    for(int n =0; n < N; n++) printf("%d", assign[n]);
+    for(int n =0; n < (K*D); n++){
+        printf("%d", c[n]);
+        printf("\n");
+    }
     
+    free(x);
+    free(c);
+    free(assign);
     return 0;
 }
