01e10162ce6c39217dfd0af3a11b3014cbf44ce6
charlloyd <char.lloyd@gmail.com>
2017-04-08 19:39:33 -0400

    Create kmeans.py

---
+++ src/kmeans.py
@@ -0,0 +1,20 @@
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+
+import pandas as pd
+
+
+##################
+### CONFIGURE ####
+##################
+
+data_fn = "../data/reviewer-data-sample.csv"
+
+##################
+### SETUP ####
+##################
+
+# import data
+data = pd.read_csv(data_fn)
+print(data)
