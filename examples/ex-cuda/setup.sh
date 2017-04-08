102e0960bf9c1b8245fa73c24327e7d39d5051c3
charlloyd <char.lloyd@gmail.com>
2017-04-07 20:43:48 -0400

    Update setup.sh

--- examples/ex-cuda/setup.sh
+++ examples/ex-cuda/setup.sh
@@ -1,3 +1,7 @@
 source new-modules.sh
 module load cuda/7.5-fasrc01
-srun --mem-per-cpu=1000 -p gpu -n 1 --gres=gpu:1 --constraint=cuda-7.5 --pty -t 0-01:00 /bin/bash
+
+rm -f cuda_hello.out
+rm -f cuda_hello.err
+rm -f cuda_square.out
+rm -f cuda_square.err
