622771855ed360b6eda4b0814762debe55b989a1
charlloyd <char.lloyd@gmail.com>
2017-04-07 20:46:25 -0400

    Update setup.sh

--- examples/ex-cuda/setup.sh
+++ examples/ex-cuda/setup.sh
@@ -5,3 +5,5 @@ rm -f cuda_hello.out
 rm -f cuda_hello.err
 rm -f cuda_square.out
 rm -f cuda_square.err
+
+-srun -p gpu -n 1 --mem-per-cpu=500 --gres=gpu:1 --constraint=cuda-7.5 --pty -t 0-01:00 /bin/bash
