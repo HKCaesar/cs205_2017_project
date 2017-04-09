fb738c46b062a4801256b0fab62493c9f4a74060
charlloyd <char.lloyd@gmail.com>
2017-04-08 20:04:33 -0400

    Update kmeans.py

--- src/kmeans.py
+++ src/kmeans.py
@@ -4,16 +4,66 @@ from pycuda.compiler import SourceModule
 
 import pandas as pd
 
-##################
+####################################
 ### CONFIGURE ####
-##################
+####################################
 
-data_fn = "../data/reviewer-data-sample.csv"
+data_fn = "../data/reviewer-data.csv"
+#data_fn = "../data/reviewer-data-sample.csv"
 
-##################
-### SETUP ####
-##################
+####################################
+### KERNELS ####
+####################################
+
+mod = SourceModule("""
+__global__ void newmeans(int *a, int *b, int *c)  {
+  int id = blockIdx.x;
+  c[id] = a[id] + b[id];
+}""")
+
+mod = SourceModule("""
+__global__ void reassign(int *a, int *b, int *c)  {
+  int id = blockIdx.x;
+  c[id] = a[id] + b[id];
+}""")
+
+####################################
+### GRAB DATA ####
+####################################
 
 # import data
 data = pd.read_csv(data_fn)
-print(data)
+data=data[:1000]
+print(len(data))
+
+####################################
+### ALLOCATE INPUT ####
+####################################
+
+# Allocate input on host
+a = numpy.array(8)
+b = numpy.array(2)
+c = numpy.array(0)
+
+a = a.astype(numpy.int32)
+b = b.astype(numpy.int32)
+c = c.astype(numpy.int32)
+
+####################################
+### ALLOCATE INPUT & COPY TO GPU ####
+####################################
+
+# Allocate on device
+a_gpu = cuda.mem_alloc(a.size * a.dtype.itemsize)
+b_gpu = cuda.mem_alloc(b.size * b.dtype.itemsize)
+c_gpu = cuda.mem_alloc(c.size * c.dtype.itemsize)
+
+# Copy from host to device
+cuda.memcpy_htod(a_gpu, a)
+cuda.memcpy_htod(b_gpu, b)
+
+####################################
+### RUN K-MEANS ####
+####################################
+
+
