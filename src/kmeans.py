b00df5eff680a76a3ace323589201c926336003a
charlloyd <char.lloyd@gmail.com>
2017-04-09 19:39:35 -0400

    Update kmeans.py

--- src/kmeans.py
+++ src/kmeans.py
@@ -35,7 +35,7 @@ __global__ void newmeans(int N, int D, int K, double *data, int *clusters, doubl
   // find the n per cluster with just one lucky thread
   if (threadIdx.x==0 & threadIdx.y==0)
   {
-    int l_clustern[tempK];
+    int l_clustern[K];
     //l_clustern = (int*)malloc(sizeof(int) * (*K));
     for(int k=0; k < (K); ++k) l_clustern[k] = 0;
     for (int n=0; n < (N); ++n) l_clustern[clusters[n]]++;
