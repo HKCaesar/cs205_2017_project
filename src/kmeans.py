c83f839bfbca1c706598a47593990e7c04988644
charlloyd <char.lloyd@gmail.com>
2017-04-09 17:26:17 -0400

    Update kmeans.py

--- src/kmeans.py
+++ src/kmeans.py
@@ -31,20 +31,20 @@ K=3
 mod = SourceModule("""
 
 __global__ void newmeans(int N, int K, double *data, double *clusters, double *means, double *clustern) {
+  
+  // find the n per cluster with just one lucky thread
   if (threadIdx.x==0 & threadIdx.y==0)
   {
     int l_clustern[K];
-    for(int k =0; k < K; ++k) l_clustern[k] = 0;
-    
-    for (int n = 0; n < N; ++n)
-    {
-      l_clustern[clusters[n]]++;
-    }
-    
+    for(int k=0; k < K; ++k) l_clustern[k] = 0;
+    for (int n=0; n < N; ++n) l_clustern[clusters[n]]++;
     for(int k =0; k < K; ++k) clustern[k] = l_clustern[k];
    }
-   
    __syncthreads();
+   
+   // sum stuff
+   
+   // divide stuff
    
   }
 
