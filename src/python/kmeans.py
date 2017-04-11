1a856764d1dcc239ad1fe78c3c3efa2cce6fdf54
Eric Dunipace <edunipace@g.harvard.edu>
2017-04-11 15:42:53 -0400

    merges

---
+++ src/python/kmeans.py
@@ -0,0 +1,112 @@
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+
+import pandas as pd
+import numpy as np
+
+import time
+import csv
+
+import string
+import functions
+
+
+
+######################################################
+### INFO ####
+######################################################
+
+# Parallel*:      Sequential:      Meaning:                 Dim:
+# data            X                reviewer data           (NxD)
+# clusters        W                cluster assignments     (Nx1)
+# means           A                means                   (KxD)
+# clustern        m                number of clusters      (1xK)
+
+# *h_ and d_ prefixes in parallel variable names indicate host vs. device copies
+
+######################################################
+### CONFIGURE ####
+######################################################
+
+data_fn = "../../data/reviewer-data.csv"
+output_dir = "../../analysis/"
+d_list = ["cunninlingus_ct_bin","fellatio_ct_bin","intercoursevaginal_ct_bin","kissing_ct_bin","manualpenilestimulation_ct_bin","massage_ct_bin"]
+
+K = 5
+limit = 1000 # impose a limit of N on the dataset
+
+
+######################################################
+### RUN K-MEANS ####
+######################################################
+
+data, initial_clusters = prep_data()
+prep_host()
+output = [['algorithm','time','convergence','distortion','n','d','k']]
+
+### Stock k-means ###
+kmeans = KMeans(n_clusters=3,n_init=100)
+kmeans.fit(data)
+print('\n-----Stock K-Means')
+print(kmeans.cluster_centers_)
+
+
+### Sequential ###
+
+start = time.time()
+seq_means, seq_clusters, seq_count, seq_distortion = sequential(data, initial_clusters)
+stop = time.time()-start
+output.append(['sequential',stop, seq_count, seq_distortion, N, D, K])
+
+print('\n-----Sequential output')
+print(seq_means)
+print(seq_clusters[:10])
+
+### Naive Parallel ###
+
+kernel1, kernel2 = pnaive_mod()
+reset_hvars()
+
+start = time.time()
+prep_device()
+kernel1(d_data, d_clusters, d_means, block=(K,D,1), grid=(1,1,1))
+#kernel2(d_data, d_clusters, d_means, d_distortion, block=(N,1,1), grid=(1,1,1))
+cuda.memcpy_dtoh(h_means, d_means)
+cuda.memcpy_dtoh(h_clusters, d_clusters)
+stop = time.time()-start
+output.append(['naive parallel',stop, '?', '?', N, D, K])
+
+print('\n-----Naive Parallel output')
+print(h_means)
+print(h_clusters[:10])
+print('Equals sequential output: %s' % str(np.array_equal(seq_means,h_means)))
+
+### Parallel Improved ###
+
+kernel1, kernel2 = pimproved_mod()
+reset_hvars()
+
+start = time.time()
+prep_device()
+#kernel1(d_data, d_clusters, d_means, block=(K,D,1), grid=(1,1,1))
+#kernel2(d_data, d_clusters, d_means, d_distortion, block=(N,1,1), grid=(1,1,1))
+cuda.memcpy_dtoh(h_means, d_means)
+cuda.memcpy_dtoh(h_clusters, d_clusters)
+stop = time.time()-start
+output.append(['improved parallel',stop, '?', '?', N, D, K])
+
+print('\n-----Improved Parallel output')
+print(h_means)
+print(h_clusters[:10])
+print('Equals sequential output: %s' % str(np.array_equal(seq_means,h_means)))
+
+######################################################
+### COPY DEVICE DATA BACK TO HOST AND COMPARE ####
+######################################################
+
+with open(output_dir + 'times.csv', 'w') as f:
+    writer = csv.writer(f, delimiter = ',')
+    writer.writerows(output)
+    f.close()
+
---
+++ src/swig/kM.i
@@ -0,0 +1,37 @@
+%module kM
+%{
+  #define SWIG_FILE_WITH_INIT
+  #include "../C/kmeans.h"
+%}
+
+%include "numpy.i"
+%init %{
+import_array();
+%}
+
+
+%apply (double *IN_ARRAY2, int DIM1, int DIM2) {(double *x, int N, int DD),(double *c, int K, int D)};
+%apply (int *IN_ARRAY1, int DIM1) {(int *assign, int NN)};
+%apply (double *ARGOUT_ARRAY1, int DIM1) {(double *out, int DDD)};
+
+%rename (kM) kM_wrap;
+%exception kM_wrap {
+    $action
+    if (PyErr_Occurred()) SWIG_fail;
+}
+%inline %{
+     void kM_wrap(double *x, int N, int DD, double *c, int K, int D, int *assign, int NN, double *out, int DDD) {
+        if (N != NN) {
+            PyErr_Format(PyExc_ValueError,
+                         "Arrays of lengths (%d,%d) given",
+                         N, NN);
+        }
+    
+        kMeans(x, c, assign, N, K, D);
+        for(int i = 0; i < (D*K); ++i) out[i] = c[i];
+
+    }
+%}
+
+
+%include "../C/kmeans.h"
