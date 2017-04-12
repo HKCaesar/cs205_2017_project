201f4776a429076949d69ac0e571f330d2908971
charlloyd <char.lloyd@gmail.com>
2017-04-12 10:32:58 -0400

    major update

--- src/python/kmeans.py
+++ src/python/kmeans.py
@@ -1,19 +1,5 @@
-import pycuda.driver as cuda
-import pycuda.autoinit
-from pycuda.compiler import SourceModule
-
-import pandas as pd
-import numpy as np
-
-import time
-import csv
-
-import string
 from functions import *
-
-from sklearn.cluster import KMeans
-
-
+import csv
 
 ######################################################
 ### INFO ####
@@ -21,9 +7,9 @@ from sklearn.cluster import KMeans
 
 # Parallel*:      Sequential:      Meaning:                 Dim:
 # data            X                reviewer data           (NxD)
-# clusters        W                cluster assignments     (Nx1)
+# labels          W                cluster assignments     (Nx1)
 # means           A                means                   (KxD)
-# clustern        m                number of clusters      (1xK)
+# clustern        m                number per cluster      (1xK)
 
 # *h_ and d_ prefixes in parallel variable names indicate host vs. device copies
 
@@ -32,83 +18,71 @@ from sklearn.cluster import KMeans
 ######################################################
 
 data_fn = "../../data/reviewer-data.csv"
-output_dir = "../../analysis/"
 d_list = ["cunninlingus_ct_bin","fellatio_ct_bin","intercoursevaginal_ct_bin","kissing_ct_bin","manualpenilestimulation_ct_bin","massage_ct_bin"]
 
-K = 5
-limit = 1000 # impose a limit of N on the dataset
+kernel_fn = "../cuda/pycumean.c"
 
+output_dir = "../../analysis/"
 
-######################################################
-### RUN K-MEANS ####
-######################################################
+K = 3
+N = 1000 # max N for review data is 118684
+D = 6 # max D for review data is 6 (we could increase this actually)
+limit = 10
 
-data, initial_clusters, N, D = prep_data(K, data_fn, d_list, limit)
-h_data, h_clusters, h_means, h_distortion = prep_host(data, initial_clusters, K, D)
 output = [['algorithm','time','convergence','distortion','n','d','k']]
 
-### Stock k-means ###
-kmeans = KMeans(n_clusters=3,n_init=100)
-kmeans.fit(data)
-print('\n-----Stock K-Means')
-print(kmeans.cluster_centers_)
+######################################################
+### PREP DATA & INITIAL LABELS ####
+######################################################
 
-
-### Sequential ###
-
-start = time.time()
-seq_means, seq_clusters, seq_count, seq_distortion = sequential(N, K, D, data, initial_clusters)
-stop = time.time()-start
-output.append(['sequential',stop, seq_count, seq_distortion, N, D, K])
-
-print('\n-----Sequential output')
-print(seq_means)
-print(seq_clusters[:10])
-
-### Naive Parallel ###
-
-kernel1, kernel2 = pnaive_mod(N, K, D)
-reset_hvars(initial_clusters, h_means, h_distortion, K, D)
-
-start = time.time()
-d_data, d_clusters, d_means, d_distortion = prep_device(h_data, h_clusters, h_means, h_distortion)
-kernel1(d_data, d_clusters, d_means, block=(K,D,1), grid=(1,1,1))
-#kernel2(d_data, d_clusters, d_means, d_distortion, block=(N,1,1), grid=(1,1,1))
-cuda.memcpy_dtoh(h_means, d_means)
-cuda.memcpy_dtoh(h_clusters, d_clusters)
-stop = time.time()-start
-output.append(['naive parallel',stop, '?', '?', N, D, K])
-
-print('\n-----Naive Parallel output')
-print(h_means)
-print(h_clusters[:10])
-print('Equals sequential output: %s' % str(np.array_equal(seq_means,h_means)))
-
-### Parallel Improved ###
-
-kernel1, kernel2 = pimproved_mod(N, K, D)
-reset_hvars(initial_clusters, h_means, h_distortion, K, D)
-
-start = time.time()
-d_data, d_clusters, d_means, d_distortion = prep_device(h_data, h_clusters, h_means, h_distortion)
-#kernel1(d_data, d_clusters, d_means, block=(K,D,1), grid=(1,1,1))
-#kernel2(d_data, d_clusters, d_means, d_distortion, block=(N,1,1), grid=(1,1,1))
-cuda.memcpy_dtoh(h_means, d_means)
-cuda.memcpy_dtoh(h_clusters, d_clusters)
-stop = time.time()-start
-output.append(['improved parallel',stop, '?', '?', N, D, K])
-
-print('\n-----Improved Parallel output')
-print(h_means)
-print(h_clusters[:10])
-print('Equals sequential output: %s' % str(np.array_equal(seq_means,h_means)))
+data, initial_labels = prep_data(data_fn, d_list, N, D, K)
 
 ######################################################
-### COPY DEVICE DATA BACK TO HOST AND COMPARE ####
+### RUN STOCK K-MEANS ####
+######################################################
+
+means, labels, distortion, runtime = stock(data, K, limit)
+
+output.append(['stock',runtime, '', distortion, N, D, K])
+print('\n-----Stock output')
+print(means)
+print(labels[:10])
+print(distortion)
+
+######################################################
+### RUN SEQUENTIAL K-MEANS ####
+######################################################
+
+means, labels, count, runtime, A1, W1 = sequential(data, initial_labels, N, D, K, limit)
+
+output.append(['sequential',runtime, count, '', N, D, K])
+print('\n-----Sequential output')
+print(means)
+print(labels[:10])
+ref_means = means
+
+print('\n-----Sequential output count == 1') # will eventually delete this
+print(A1)
+print(W1[:10])
+ref_means = A1
+
+######################################################
+### RUN PARALLEL K-MEANS ####
+######################################################
+
+means, labels, count, runtime = parallel(data, initial_labels, kernel_fn, N, K, D, limit)
+
+output.append(['parallel',runtime, count, '', N, D, K])
+print('\n-----Parallel output')
+print(means)
+print(labels[:10])
+print('Equals stock means: %s' % str(np.array_equal(ref_means,means)))
+
+######################################################
+### WRITE DATA TO CSV ####
 ######################################################
 
 with open(output_dir + 'times.csv', 'w') as f:
     writer = csv.writer(f, delimiter = ',')
     writer.writerows(output)
     f.close()
-
