65e05f0e5bc7ffd2a84b7d83406e8c3c2890b91a
kareemcarr <kareem.c.carr@gmail.com>
2017-04-15 15:50:51 -0400

    printing data

--- plan_b/kmeans/mpi_kmeans.py
+++ plan_b/kmeans/mpi_kmeans.py
@@ -38,7 +38,9 @@ def mpi_kmeans(data, n_clusters,max_iter=100):
     allocations,labels = partition(labels,size)
     labels = comm.scatter(labels, root=0)
 
-    data   = data[labels]
+    data = data[labels]
+
+    print("rank: %d, data: %r",rank, data)
 
     for k in range(max_iter):
 
