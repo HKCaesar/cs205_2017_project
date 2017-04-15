c07d5a10723a29b7b8b97977fa50094f81e3731b
kareemcarr <kareem.c.carr@gmail.com>
2017-04-15 19:31:52 -0400

    printing data

--- plan_b/test.py
+++ plan_b/test.py
@@ -8,5 +8,5 @@ from kmeans.mpi_kmeans import mpi_kmeans
 
 centers,labels,timing=mpi_kmeans(data=data, n_clusters=3)
 
-#print(distortion(labels,centers,data))
+print(distortion(labels,centers,data))
 
