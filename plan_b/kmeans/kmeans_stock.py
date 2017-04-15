4041701e38e563d6953fa8d8f3c566a3a0f16f86
kareemcarr <kareem.c.carr@gmail.com>
2017-04-15 14:10:49 -0400

    kmeans package file

---
+++ plan_b/kmeans/kmeans_stock.py
@@ -0,0 +1,9 @@
+import time
+from sklearn.cluster import KMeans
+
+def kmeans_stock(data, n_clusters,max_iter=100):
+    start = time.time()
+    stock = KMeans(n_clusters=n_clusters,n_init=1,max_iter=max_iter,algorithm='full')
+    stock.fit(data)
+    timing = time.time()-start
+    return stock.cluster_centers_,stock.labels_,timing
