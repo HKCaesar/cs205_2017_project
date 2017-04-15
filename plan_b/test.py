fdd6f34a7cbf9c6d6c2ca7ced933d20d14fcd7ed
kareemcarr <kareem.c.carr@gmail.com>
2017-04-15 14:07:42 -0400

    add test

---
+++ plan_b/test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from kmeans.utilities import distortion
+
+data = pd.read_csv('../data/yeast.csv', header=None).values
+
+
+from kmeans.mpi_kmeans import mpi_kmeans
+
+centers,labels,timing=mpi_kmeans(data=data, n_clusters=3)
+
+print(distortion(labels,centers,data))
+
