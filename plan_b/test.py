a11d256ab334baffcebe7227e2f5ed01815e566f
kareemcarr <kareem.c.carr@gmail.com>
2017-04-15 21:12:50 -0400

    printing data

--- plan_b/test.py
+++ plan_b/test.py
@@ -10,8 +10,8 @@ from kmeans.kmeans_stock import kmeans_stock
 print("")
 
 centers,labels,timing=mpi_kmeans(data=data, n_clusters=2)
-print("mpi distortion: %f time: %f" % (distortion(labels,centers,data),timing))
+print("mpi distortion: %f time: %f s" % (distortion(labels,centers,data),timing))
 
 centers,labels,timing= kmeans_stock(data=data,n_clusters=2)
-print("stock distortion: %f time: %f" % (distortion(labels,centers,data),timing))
+print("stock distortion: %f time: %f s" % (distortion(labels,centers,data),timing))
 
