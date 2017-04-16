e9d760bc691d5276c31fb9f044118b32b950e9f1
kareemcarr <kareem.c.carr@gmail.com>
2017-04-15 21:13:56 -0400

    printing data

--- plan_b/test.py
+++ plan_b/test.py
@@ -7,9 +7,8 @@ data = pd.read_csv('../data/yeast.csv', header=None).values
 from kmeans.mpi_kmeans import mpi_kmeans
 from kmeans.kmeans_stock import kmeans_stock
 
-print("")
-
 centers,labels,timing=mpi_kmeans(data=data, n_clusters=2)
+print("")
 print("mpi distortion: %f time: %f s" % (distortion(labels,centers,data),timing))
 
 centers,labels,timing= kmeans_stock(data=data,n_clusters=2)
