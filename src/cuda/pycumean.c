d6fd14a2db8246259ecb860abe03def9305f2297
charlloyd <char.lloyd@gmail.com>
2017-04-15 23:07:29 -0400

    Update pycumean.c

--- src/cuda/pycumean.c
+++ src/cuda/pycumean.c
@@ -52,17 +52,13 @@ __global__ void reassign(double *data, int *labels, double *means, int *conv_arr
     int k = threadIdx.x;
     int d = threadIdx.y;
     int n = blockIdx.x;
-    int wInBlockid = d + k * D;
-    int tid = d + k * D + n * K*D;
+    int tid = d + k * D;
     int dataid = d + n * D;
     double min;
     int min_idx;
-    __shared__ int s_conv[N];
-    
-    if(tid == 0) (*conv) = 1;
-    
+        
     // get KxD squares
-        s_squares[wInBlockid] = (data[dataid] - means[wInBlockid]) * (data[dataid] - means[wInBlockid]);
+        s_squares[tid] = (data[dataid] - means[tid]) * (data[dataid] - means[tid]);
         __syncthreads();
     
     // add KxD squares to get K sums using K lucky threads
@@ -74,7 +70,8 @@ __global__ void reassign(double *data, int *labels, double *means, int *conv_arr
     __syncthreads();
     
     // check for the minimum of the K sums using 1 lucky thread per block
-        if (wInBlockid == 0) {
+        if (tid == 0) {
+          conv_array[n] = 0;
           min = s_sums[0];
           min_idx = 0;
           for(int kk = 1; kk < K; ++kk) {
@@ -84,33 +81,10 @@ __global__ void reassign(double *data, int *labels, double *means, int *conv_arr
             }
           }          
           if (labels[n] != min_idx) {
-            conv_array[n] = 0;
+            conv_array[n] = 1;
             labels[n] = min_idx;
           }
         }
     __syncthreads();
 
-    //check convergence
-    if(tid < N){
-        s_conv[wInBlockid] = conv[tid];
-    }
-        __syncthreads();
-    
-    if(tid < N){
-        for(int kk = (K*D)/2; kk > 0; kk>>=1)
-        {
-            double temp = s_conv[wInBlockid + kk];
-            if(temp <  s_conv[wInBlockid]) s_conv[wInBlockid] = temp;
-            __syncthreads();
-        }
-        if(wInBlockid == 0) conv_array[n] = s_conv[0];
-    }
-    __syncthreads();
-    if(tid == 0){
-        for(int kk = 0; kk < N/(K*D); kk++){
-            (*conv) = ((*conv) > conv_array[n] ? 0 : 1);
-            if(!(*conv)) break;
-        }
-    }
-     __syncthreads();
 }
