06035ee5de86818cb5ecd5d908aa76c0a99fcd2c
charlloyd <char.lloyd@gmail.com>
2017-04-15 21:50:11 -0400

    Update pycumean.c

--- src/cuda/pycumean.c
+++ src/cuda/pycumean.c
@@ -64,7 +64,7 @@ __global__ void reassign(double *data, double *labels, double *means, int *conv_
     // get KxD squares
         s_squares[wInBlockid] = (data[dataid] - means[wInBlockid]) * (data[dataid] - means[wInBlockid]);
         
-        printf("winid: %d value: %d ",wInBlockid, s_squares[wInBlockid])
+        printf("winid: %d value: %d ",wInBlockid, s_squares[wInBlockid]);
     
         __syncthreads();
     
