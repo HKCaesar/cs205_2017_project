afa948cf77607ccb2c1d0df596fd24e3d985607d
kareemcarr <kareem.c.carr@gmail.com>
2017-04-15 21:06:22 -0400

    printing data

--- plan_b/kmeans/kmeans_stock.py
+++ plan_b/kmeans/kmeans_stock.py
@@ -3,7 +3,7 @@ from sklearn.cluster import KMeans
 
 def kmeans_stock(data, n_clusters,max_iter=100):
     start = time.time()
-    stock = KMeans(n_clusters=n_clusters,n_init=1,max_iter=max_iter,algorithm='full')
+    stock = KMeans(n_clusters=n_clusters,n_init=1,max_iter=max_iter)
     stock.fit(data)
     timing = time.time()-start
     return stock.cluster_centers_,stock.labels_,timing
--- plan_b/test.py
+++ plan_b/test.py
@@ -7,9 +7,9 @@ data = pd.read_csv('../data/yeast.csv', header=None).values
 from kmeans.mpi_kmeans import mpi_kmeans
 from kmeans.kmeans_stock import kmeans_stock
 
-centers,labels,timing= kmeans_stock(data=data,n_clusters=2)
+centers,labels,timing=mpi_kmeans(data=data, n_clusters=2)
 print(distortion(labels,centers,data))
 
-centers,labels,timing=mpi_kmeans(data=data, n_clusters=2)
+centers,labels,timing= kmeans_stock(data=data,n_clusters=2)
 print(distortion(labels,centers,data))
 
