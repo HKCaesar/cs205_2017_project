4e590cf17a33d9f23bfac840e46a816ea6b84f7c
Eric Dunipace <eifer@Cid.local>
2017-04-29 04:14:58 -0400

    push

--- src/python/functions.py
+++ src/python/functions.py
@@ -7,16 +7,14 @@ import csv
 ######################################################
 
 def prep_data(data_fn, d_list, N, D, K):
-
     # import data file and subset data for k-means
     reviewdata = pd.read_csv(data_fn)
+    data = pd.read_csv(data_fn)
     for n in range(int(N/118684)):
-        reviewdata = reviewdata.append(reviewdata)
+        reviewdata = reviewdata.append(data)
         print(len(reviewdata))
-        if len(reviewdata) > N: break
-    data = reviewdata[d_list[:D]][:N].values
+    data = data[d_list[:D]][:N].values
     data = np.ascontiguousarray(data, dtype=np.float64)
-
     # assign random clusters & shuffle
     initial_labels = np.ascontiguousarray(np.zeros(N,dtype=np.intc, order='C'))
     for n in range(N):
@@ -26,7 +24,6 @@ def prep_data(data_fn, d_list, N, D, K):
         temp = initial_labels[j]
         initial_labels[j] = initial_labels[i]
         initial_labels[i] = temp
-
     return data, initial_labels
 
 ######################################################
