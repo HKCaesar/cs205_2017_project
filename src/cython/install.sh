71b37143e83d545792245abc799c18398ce9c050
kareemcarr <kareem.c.carr@gmail.com>
2017-05-02 00:51:36 -0400

    install.sh

--- src/cython/install.sh
+++ src/cython/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python setup.py build_ext --inplace
+python setup2.py build_ext --inplace
---
+++ src/cython/setup2.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+import numpy
+
+### list all .pyx files
+
+our_modules = [
+    Extension("cython_kmeans", ["cython_kmeans.pyx", "../C/cython_kmeans.c"], language="c", extra_compile_args=['-O3'], extra_link_args=['-std=c11'], include_dirs=[numpy.get_include()])
+]
+
+### apparently equivalent ways to do setup
+
+setup(name = 'kmeansCPY', ext_modules=cythonize(our_modules))
+#setup(name = 'hw1', cmdclass = {'build_ext': build_ext}, ext_modules = our_modules)
