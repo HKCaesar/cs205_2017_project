eb91a847663c001323c7543ff85eb6041502abdf
kareemcarr <kareem.c.carr@gmail.com>
2017-05-02 01:21:04 -0400

    .

--- src/cython/cython_kmeans.pyx
+++ src/cython/cython_kmeans.pyx
@@ -27,20 +27,20 @@ def cython_kmeans(double [:,:] data, int [:] initial_labels, int N, int D, int K
         converged = 1
 
         # compute centers
-        for k in prange(K):
+        for k in range(K):
             for d in prange(D):
                 centers[k, d] = 0
             clustern[k] = 0
-        for n in prange(N):
+        for n in range(N):
             for d in range(D):
                 centers[labels[n], d] += data[n, d]
             clustern[labels[n]] += 1
-        for k in prange(K):
+        for k in range(K):
             for d in range(D):
                 centers[k, d] = centers[k, d] / clustern[k]
 
         # assign to closest center
-        for n in prange(N):
+        for n in range(N):
             min_val = np.inf
             min_ind = -1
             for k in range(K):
